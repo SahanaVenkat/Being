9702436052aa9cb0b3df7278d39d8f3cfe6b1035
Sahana V <sahanavenkat10@gmail.com>
2020-11-04 15:58:15 +0530

    Yoga card done

---
+++ lib/widgets/cards/yoga_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+Widget yogaCard(
+    BuildContext context, String yogaPose, String poseName, String benefit, Color cardColor) {
+  return Container(
+      height: MediaQuery.of(context).size.height * 0.18,
+      padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.008),
+      margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            yogaPose,
+            fit: BoxFit.fitHeight,
+          ),
+          Text(
+            poseName,
+            maxLines: 1,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * 0.018,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            benefit,
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * 0.015,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ));
+}
