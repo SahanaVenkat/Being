c578500a3638faeb30d89c4c6e130d0b309c70a1
Abhishek Sunkewar <abhisheksunkewar@icloud.com>
2020-11-07 16:17:31 +0530

    increase font size

--- lib/widgets/cards/yoga_card.dart
+++ lib/widgets/cards/yoga_card.dart
@@ -1,39 +1,40 @@
 import 'package:flutter/material.dart';
 
-Widget yogaCard(
-    BuildContext context, String yogaPose, String poseName, String benefit, Color cardColor) {
+Widget yogaCard(BuildContext context, String yogaPose, String poseName,
+    String benefit, Color cardColor) {
   return Container(
-      height: MediaQuery.of(context).size.height * 0.18,
-      padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.008),
-      margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Image.asset(
-            yogaPose,
-            fit: BoxFit.fitHeight,
+    height: MediaQuery.of(context).size.height * 0.18,
+    padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.008),
+    margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+    decoration: BoxDecoration(
+      color: cardColor,
+      borderRadius: BorderRadius.circular(15.0),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          yogaPose,
+          fit: BoxFit.fitHeight,
+        ),
+        Text(
+          poseName,
+          maxLines: 1,
+          overflow: TextOverflow.clip,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.height * 0.022,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          Text(
-            poseName,
-            maxLines: 1,
-            overflow: TextOverflow.clip,
-            style: TextStyle(
-              fontSize: MediaQuery.of(context).size.height * 0.018,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        ),
+        Text(
+          benefit,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.height * 0.018,
+            color: Colors.white,
           ),
-          Text(
-            benefit,
-            style: TextStyle(
-              fontSize: MediaQuery.of(context).size.height * 0.015,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ));
+        ),
+      ],
+    ),
+  );
 }
